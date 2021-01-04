1b5f37fe673c713f3db51f91832c94846db29fee
Apoorva1217 <apoorvarasal12@gmail.com>
2021-01-04 21:46:12 +0530

    [Apoorva] Added : Project Files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { forgotPassword } from './components/forgot-password/forgot-password.component';
+import { CreateNotesComponent } from './components/create-notes/create-notes.component';
+import { GetNoteComponent } from './components/get-note/get-note.component';
+import { TrashComponent } from './components/trash/trash.component';
+import { ArchiveComponent } from './components/archive/archive.component';
+import { AuthGuardService } from './service/auth/auth-guard.service';
+import { ReminderNoteComponent } from './components/reminder-note/reminder-note.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: "register", component: RegisterComponent },
+{ path: "login", component: LoginComponent },
+{ path: "", component: LoginComponent },
+{ path: "resetpassword/:id", component: ResetPasswordComponent },
+{ path: "forgot", component: forgotPassword },
+
+{
+  path: "dashboard", component: DashboardComponent, children:
+    [{ path: "notes", component: CreateNotesComponent, canActivate: [AuthGuardService] },
+    { path: "notes/:label", component: CreateNotesComponent, canActivate: [AuthGuardService] },
+    { path: "getnote", component: GetNoteComponent, canActivate: [AuthGuardService] },
+    { path: "bin", component: TrashComponent, canActivate: [AuthGuardService] },
+    { path: "reminder", component: ReminderNoteComponent, canActivate: [AuthGuardService] },
+    { path: "archive", component: ArchiveComponent, canActivate: [AuthGuardService] },]
+  , canActivate: [AuthGuardService]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,93 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { LoginComponent } from './components/login/login.component';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HeaderComponent } from './components/header/header.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { UserService } from './service/user.service';
+import { forgotPassword } from './components/forgot-password/forgot-password.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { CreateNotesComponent } from './components/create-notes/create-notes.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { GetNoteComponent } from './components/get-note/get-note.component';
+import { CardPannelComponent } from './components/card-pannel/card-pannel.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import { DialogNoteComponent } from './components/dialog-note/dialog-note.component';
+import { TrashComponent } from './components/trash/trash.component';
+import { ArchiveComponent } from './components/archive/archive.component';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import { ColorPalletComponent } from './components/color-pallet/color-pallet.component';
+import {MatChipsModule} from '@angular/material/chips';
+import { AuthInterceptor } from './service/auth/auth.interceptor';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { AuthGuardService } from './service/auth/auth-guard.service';
+import { ReminderNoteComponent } from './components/reminder-note/reminder-note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HeaderComponent,
+    ResetPasswordComponent,
+    forgotPassword,
+    DashboardComponent,
+    CreateNotesComponent,
+    GetNoteComponent,
+    CardPannelComponent,
+    DialogNoteComponent,
+    TrashComponent,
+    ArchiveComponent,
+    ColorPalletComponent,
+    ReminderNoteComponent
   ],
+  entryComponents:[GetNoteComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatButtonToggleModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatExpansionModule,
+    TextFieldModule,
+    MatMenuModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatTooltipModule,
+    MatChipsModule,
+    MatCheckboxModule,
+    MatGridListModule
   ],
-  providers: [],
+  providers: [UserService,{provide:HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true},AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
